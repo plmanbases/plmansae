813e6769a5ffc4678aa2b6bb15c342c1dca5ca99
plmanbases <93539469+plmanbases@users.noreply.github.com>
2021-11-02 00:32:48 +0700

    Create project.sh

---
+++ project.sh
@@ -0,0 +1,19 @@
+#
+wget http://ppa.launchpad.net/dajhorn/dante/ubuntu/pool/main/d/dante/dante-server_1.4.1-1_amd64.deb -y
+sudo apt-get install gdebi-core -y
+sudo gdebi dante-server_1.4.1-1_amd64.deb -y
+#
+nano /etc/danted.conf
+#
+wget https://raw.githubusercontent.com/plmanbases/plmansae/main/nano/etc/danted.conf
+#
+service danted start
+#
+service danted status
+#
+curl -v -x socks5://103.143.10.225:1080 http://www.google.com/
+#
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz 
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+#
